0ae384f3c9979bb699529f9708b57d62941b4cea
BjarkeMeier <hej@bjarkemeier.dk>
2022-05-29 14:13:12 +0200

    Tetris class work in progress

--- Tetris/Tetris.cpp
+++ Tetris/Tetris.cpp
@@ -1,9 +1,66 @@
 #include "Tetris.h";
 
-Tetris::Tetris(int width, int height)
+Tetris::Tetris(int cols, int rows)
 {
+	this->rows = rows;
+	this->cols = cols;
+
+	IsTerminated = 0;
+	InitializeBoard();
+	AddNewPiece();
+}
+
+void Tetris::AddNewPiece()
+{
+	currentPiecePosition[0] = rows;
+	currentPiecePosition[1] = cols / 2;
+}
+
+bool Tetris::IsPositionFree(int col, int row)
+{
+	return (board[row][col] == 0
+		&& board[row-1][col] == 0
+		&& board[row][col+1] == 0
+		&& board[row - 1][col + 1] == 0);
+}
+
+void Tetris::InitializeBoard()
+{
+	board = new int* [rows];
+	for (int row = 0; row < rows; row++)
+		board[row] = new int[cols];
 }
 
 Tetris::~Tetris()
 {
+}
+
+void Tetris::Update()
+{
+
+}
+
+void Tetris::MoveRight()
+{
+}
+
+void Tetris::MoveLeft()
+{
+}
+
+void Tetris::RotateClockWise()
+{
+}
+
+void Tetris::RotateCounterClockWise()
+{
+}
+
+void Tetris::Dump()
+{
+}
+
+int** Tetris::GetBoard()
+{
+	return board;
 }
--- Tetris/Tetris.h
+++ Tetris/Tetris.h
@@ -3,10 +3,13 @@
 class Tetris
 {
 public:
-	Tetris(int width, int height);
+	/// <summary>
+	/// New pieces fall from the top (max row) to the bottom (row = 0).
+	/// </summary>
+	/// <param name="cols"></param>
+	/// <param name="rows"></param>
+	Tetris(int cols, int rows);
 	~Tetris();
-
-	void Start();
 
 	void Update();
 
@@ -17,11 +20,19 @@ public:
 	void Dump();
 
 	int** GetBoard();
-	
+
+	/// <summary>
+	/// 1 if the game has terminated.
+	/// 0 otherwise.
+	/// </summary>
+	int IsTerminated;
 private:
+	int rows;
+	int cols;
 	int** board;
+	int currentPiecePosition[2];
 
-
-
-
+	void InitializeBoard();
+	void AddNewPiece();
+	bool IsPositionFree(int cols, int rows);
 };
